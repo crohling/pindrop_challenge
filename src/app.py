c3f744ad4cd94595c4aa41a7045271862366ed71
Christian Rohling <crohling88@gmail.com>
2015-10-15 22:03:00 -0400

    Added support for redis if wanted

--- src/app.py
+++ src/app.py
@@ -6,6 +6,7 @@ import itertools
 # I would normally use the requests library instead of urllib2
 import urllib2
 import argparse
+import redis
 from handlers import PhoneHandler
 from parsing import parse_page
 
@@ -24,14 +25,27 @@ def get_data(args):
         raise Exception("Content of page unable to be read")
     return content
 
+def load_redis(database, results):
+    for item in results:
+        database.set("area_code|%s" % item.get("area_code"), json.dumps(item))
+        database.set("phone_number|%s" % item.get("phone_number"), json.dumps(item))
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Serve an API to 800notes front page data')
     parser.add_argument('--file', type=argparse.FileType('r'))
+    parser.add_argument('-r', action="store_true")
     args = parser.parse_args()
     content = get_data(args)
     results = parse_page(content)
+    if args.r:
+        print "Serving data out of redis"
+        database = redis.StrictRedis(host='redis', port=6379, db=0)
+        load_redis(database, results)
+    else:
+        print "Serving data out of memory"
+        database = results
     application = tornado.web.Application([
-        (r"/(phone_number/([0-9]{10})?|area_code/([0-9]{3})?)", PhoneHandler, dict(number_database=results)),
+        (r"/(phone_number/([0-9]{10})?|area_code/([0-9]{3})?)", PhoneHandler, dict(number_database=database, is_redis=args.r)),
     ])
     application.listen(8000)
     tornado.ioloop.IOLoop.current().start()
--- src/handlers.py
+++ src/handlers.py
@@ -2,19 +2,32 @@ import tornado.web
 import json
 
 class PhoneHandler(tornado.web.RequestHandler):
-    def initialize(self, number_database):
+    def initialize(self, number_database, is_redis=False):
         self.database = number_database
+        self.is_redis = is_redis
 
     def get(self, field_and_value, *args):
         field, value = field_and_value.split("/")
         if value:
-            value_document = [doc for doc in self.database if doc.get(field) == value]
+            value_document = None
+            if self.is_redis:
+                value_document = json.loads(self.database.get("%s|%s" % (field, value)))
+            else:
+                value_document = [doc for doc in self.database if doc.get(field) == value]
             if value_document:
-                data = json.dumps(value_document)
+                data = value_document
             else:
                 raise tornado.web.HTTPError(404)
         else:
-            data = json.dumps(self.database)
-        self.write(data)
+            if self.is_redis:
+                data = []
+                keys = self.database.keys('*')
+                for key in keys:
+                    data.append(json.loads(self.database.get(key)))
+            else:
+                data = self.database
+        print data, type(data)
+        self.set_header('Content-Type', 'application/json')
+        self.write(json.dumps(data))
         return
 
